6533d0d12577b77e73aa0c1732f04f6acc435561
2manydaves3003 <91045919+2manydaves3003@users.noreply.github.com>
2022-02-16 22:35:04 -0800

    Updates to TestFTClib project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyRobot/SWTestBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyRobot/SWTestBot.java
@@ -1,29 +1,69 @@
 package org.firstinspires.ftc.teamcode.MyRobot;
 
 import com.arcrobotics.ftclib.command.Robot;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Subsystems.MecanumDriveTrain;
 
 public class SWTestBot extends Robot {
-
     HardwareMap m_hardwareMap;
     Telemetry m_telemetry;
+
+    //DriveTrain motors
+    Motor m_frontLeftMotor, m_frontRightMotor, m_backLeftMotor, m_backRightMotor;
+    MecanumDriveTrain m_driveTrain;
 
     public SWTestBot(OpModeType type, HardwareMap hardwareMap, Telemetry telemetry)
     {
         m_hardwareMap = hardwareMap;
         m_telemetry = telemetry;
 
+        initializeDriveTrain();
+        initializeOpMode(type);
+    }
+
+    private void initializeDriveTrain()
+    {
+        m_frontLeftMotor = new Motor(m_hardwareMap, "FL");
+        m_frontRightMotor = new Motor(m_hardwareMap, "FR");
+        m_backLeftMotor = new Motor(m_hardwareMap, "BL");
+        m_backRightMotor = new Motor(m_hardwareMap, "BR");
+
+        m_frontRightMotor.setInverted(true);
+        m_backRightMotor.setInverted(true);
+
+        m_frontLeftMotor.setRunMode(Motor.RunMode.VelocityControl);
+        m_frontRightMotor.setRunMode(Motor.RunMode.VelocityControl);
+        m_backLeftMotor.setRunMode(Motor.RunMode.VelocityControl);
+        m_backRightMotor.setRunMode(Motor.RunMode.VelocityControl);
+
+        m_driveTrain = new MecanumDriveTrain(m_frontLeftMotor, m_frontRightMotor,m_backLeftMotor,m_backRightMotor);
+    }
+
+    private void initializeOpMode(OpModeType type)
+    {
         if(type == OpModeType.TELEOP)
         {
             m_telemetry.addData("Initialize","TeleOp");
-            m_telemetry.update();
             initializeTeleOp();
+        }
+        else if (type == OpModeType.AUTO)
+        {
+            m_telemetry.addData("Initialize", "Auton");
+            initializeAuton();
         }
     }
 
-    private void initializeTeleOp() {
+    private void initializeTeleOp()
+    {
+
+    }
+
+    private void initializeAuton()
+    {
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MecanumDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/MecanumDriveTrain.java
@@ -1,8 +1,64 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveWheelSpeeds;
 
-public class MecanumDriveTrain {
+public class MecanumDriveTrain extends SubsystemBase {
 
-//    MecanumDrive m_drive = new MecanumDrive()
+    Motor m_frontLeftMotor;
+    Motor m_frontRightMotor;
+    Motor m_backLeftMotor;
+    Motor m_backRightMotor;
+
+    ChassisSpeeds m_speeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+    Rotation2d m_gyroAngle = new Rotation2d(0.0);
+
+    // Locations of the wheels relative to the robot center.
+    Translation2d m_frontLeftLocation =
+            new Translation2d(0.381, 0.381);
+    Translation2d m_frontRightLocation =
+            new Translation2d(0.381, -0.381);
+    Translation2d m_backLeftLocation =
+            new Translation2d(-0.381, 0.381);
+    Translation2d m_backRightLocation =
+            new Translation2d(-0.381, -0.381);
+
+    // Creating my kinematics object using the wheel locations.
+    MecanumDriveKinematics m_kinematics = new MecanumDriveKinematics(
+                    m_frontLeftLocation, m_frontRightLocation,
+                    m_backLeftLocation, m_backRightLocation);
+
+    public MecanumDriveTrain(Motor frontLeft, Motor frontRight, Motor backLeft, Motor backRight)
+    {
+        m_frontLeftMotor = frontLeft;
+        m_frontRightMotor = frontRight;
+        m_backLeftMotor = backLeft;
+        m_backRightMotor = backRight;
+    }
+
+    public void driveFieldCentric(ChassisSpeeds speeds, Rotation2d gyroAngle) {
+        m_speeds = speeds;
+        m_gyroAngle = gyroAngle;
+    }
+
+    @Override
+    public void periodic()
+    {
+        ChassisSpeeds robotSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+                m_speeds.vxMetersPerSecond,
+                m_speeds.vyMetersPerSecond,
+                m_speeds.omegaRadiansPerSecond,
+                m_gyroAngle);
+
+        MecanumDriveWheelSpeeds wheelSpeeds =
+                m_kinematics.toWheelSpeeds(robotSpeeds);
+
+        //m_frontLeftMotor.set();
+    }
 }
