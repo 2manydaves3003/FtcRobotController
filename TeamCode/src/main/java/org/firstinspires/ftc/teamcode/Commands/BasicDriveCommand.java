620350122c353d7b01bad1f893c07644ac89ccc0
2manydaves3003 <91045919+2manydaves3003@users.noreply.github.com>
2022-02-22 21:28:50 -0800

    Updated drive command with more correct drivetrain scaling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/BasicDriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/BasicDriveCommand.java
@@ -4,6 +4,7 @@ import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.ChassisSpeeds;
 
+import org.firstinspires.ftc.teamcode.MyRobot.Constants;
 import org.firstinspires.ftc.teamcode.Subsystems.MecanumDriveTrain;
 import org.firstinspires.ftc.teamcode.Subsystems.RobotLocalization;
 
@@ -31,12 +32,12 @@ public class BasicDriveCommand extends CommandBase {
     @Override
     public void execute()
     {
-        double MULTIPLIER = 10;
+        double speedFactor = 0.8;
         //Field-centric transformation placed here, but could go in DriveTrain
         ChassisSpeeds robotCentricSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
-                m_xSupplier.getAsDouble() * MULTIPLIER,
-                m_ySupplier.getAsDouble() * MULTIPLIER,
-                m_omegaSupplier.getAsDouble() * MULTIPLIER,
+                m_xSupplier.getAsDouble() * Constants.SWTestBot.MAXVEL * speedFactor,
+                m_ySupplier.getAsDouble() * Constants.SWTestBot.MAXVEL * speedFactor,
+                m_omegaSupplier.getAsDouble() * Constants.SWTestBot.MAXVEL * speedFactor,
                 Rotation2d.fromDegrees(0.0));
 
         m_drivetrain.drive(robotCentricSpeeds);
